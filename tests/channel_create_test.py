6b6d66b87e6673c84033bfb6aeb4c9e9f9862911
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2022-03-01 10:28:07 +1100

    Fix: correct a variable used in last two tests

--- tests/channel_create_test.py
+++ tests/channel_create_test.py
@@ -25,16 +25,16 @@ def test_create_invalid_channel_longname(example_user_id):
 def test_create_channel_public(example_user_id):
     new_channel_id = channels_create_v1(example_user_id, "I_love_seams", True)
     store = data_store.get()
-    assert data_store['channels'][0]['channel_id'] == new_channel_id
-    assert data_store['channels'][0]['name'] == "I_love_seams"
-    assert data_store['channels'][0]['is_public'] == True
-    assert data_store['channels'][0]['user_ids'][0] == example_user_id
+    assert store['channels'][0]['channel_id'] == new_channel_id
+    assert store['channels'][0]['name'] == "I_love_seams"
+    assert store['channels'][0]['is_public'] == True
+    assert store['channels'][0]['user_ids'][0] == example_user_id
 
 
 def test_create_channel_private(example_user_id):
     new_channel_id = channels_create_v1(example_user_id, "Badgers", False)
     store = data_store.get()
-    assert data_store['channels'][0]['channel_id'] == new_channel_id
-    assert data_store['channels'][0]['name'] == "Badgers"
-    assert data_store['channels'][0]['is_public'] == False
-    assert data_store['channels'][0]['user_ids'][0] == example_user_id
+    assert store['channels'][0]['channel_id'] == new_channel_id
+    assert store['channels'][0]['name'] == "Badgers"
+    assert store['channels'][0]['is_public'] == False
+    assert store['channels'][0]['user_ids'][0] == example_user_id
