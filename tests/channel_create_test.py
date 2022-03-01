296926a37bd9062022c639a7022da133be6e7ec6
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2022-03-01 11:02:05 +1100

    Fix: correctly access return type of create channel function

--- tests/channel_create_test.py
+++ tests/channel_create_test.py
@@ -25,7 +25,7 @@ def test_create_invalid_channel_longname(example_user_id):
 def test_create_channel_public(example_user_id):
     new_channel_id = channels_create_v1(example_user_id, "I_love_seams", True)
     store = data_store.get()
-    assert store['channels'][0]['channel_id'] == new_channel_id
+    assert store['channels'][0]['channel_id'] == new_channel_id.get('channel_id')
     assert store['channels'][0]['name'] == "I_love_seams"
     assert store['channels'][0]['is_public'] == True
     assert store['channels'][0]['user_ids'][0] == example_user_id
@@ -34,7 +34,7 @@ def test_create_channel_public(example_user_id):
 def test_create_channel_private(example_user_id):
     new_channel_id = channels_create_v1(example_user_id, "Badgers", False)
     store = data_store.get()
-    assert store['channels'][0]['channel_id'] == new_channel_id
+    assert store['channels'][0]['channel_id'] == new_channel_id.get('channel_id')
     assert store['channels'][0]['name'] == "Badgers"
     assert store['channels'][0]['is_public'] == False
     assert store['channels'][0]['user_ids'][0] == example_user_id
