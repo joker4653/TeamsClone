b145abbe89948047c8c89807eeaba2c414737989
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2022-03-01 17:34:23 +1100

    dev: added test to check for invalid user_id

--- tests/channel_create_test.py
+++ tests/channel_create_test.py
@@ -3,7 +3,7 @@ import pytest
 from src.channels import channels_create_v1
 from src.auth import auth_register_v1, auth_login_v1
 from src.other import clear_v1
-from src.error import InputError
+from src.error import AccessError, InputError
 from src.data_store import data_store
 
 @pytest.fixture
@@ -19,6 +19,12 @@ def test_create_invalid_channel_shortname(example_user_id):
 def test_create_invalid_channel_longname(example_user_id):
     with pytest.raises(InputError):
         channels_create_v1(example_user_id, "names_>_20_are_grosss", True)
+
+def test_create_channel_bad_user_id(example_user_id):
+    with pytest.raises(AccessError):
+        channels_create_v1(67, "good_name", True)
+    with pytest.raises(AccessError):
+        channels_create_v1("bad_id", "good_name", True)
 
 # Asserts in the following two tests check that the data structure 'channels' 
 # is updated correctly when a new channel is created.
