3892f74001251ce8d1d4a32db31855a1567e7d0f
Nicholas Maiden <z5256318@ad.unsw.edu.au>
2022-03-03 17:59:04 +1100

    fix: add more tests

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -33,15 +33,20 @@ def test_create_channel_single(example_user_id):
     new_channel_id = channels_create_v1(example_user_id, "I_love_seams", True)
     assert new_channel_id.get('channel_id') == 1
 
-def test_create_channel_duplicate(example_user_id):
+def test_create_channel_duplicate_same_user(example_user_id):
     new_channel_id = channels_create_v1(example_user_id, "I_love_seams", True)
     new_channel_id = channels_create_v1(example_user_id, "I_love_seams", True)
     assert new_channel_id.get('channel_id') == 2
 
+def test_create_channel_duplicate_diff_user(example_user_id):
+    new_channel_id = channels_create_v1(example_user_id, "I_love_seams", True)
+    user_id2 = auth_register_v1("james.smith12@gmail.com", "my_good_password1", "James", "Smith")
+    new_channel_id = channels_create_v1(user_id2.get('auth_user_id'), "I_love_seams", True)
+    assert new_channel_id.get('channel_id') == 2
 
 def test_create_channel_multiple(example_user_id):
     channels_create_v1(example_user_id, "Badgers", False)
-    user_id2 = auth_register_v1("steve.smith12@gmail.com", "my_good_password1", "James", "Smith")
+    user_id2 = auth_register_v1("james.smith12@gmail.com", "my_good_password1", "James", "Smith")
     channels_create_v1(user_id2.get('auth_user_id'), "I_love_seams", True)
     user_id3 = auth_register_v1("carl.johns56@gmail.com", "my_good_password1", "Carl", "Johns")
     channels_create_v1(user_id3.get('auth_user_id'), "cool_channel", True)
