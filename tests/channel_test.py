66d79fb0e6ed40a0189f878c7a3fb5106eeda101
z5364817 <z5364817@cse.unsw.edu.au>
2022-03-05 00:03:50 +1100

    test: tests for channel_details_v1

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -10,11 +10,11 @@ from src.error import AccessError, InputError
 def example_user_id() -> list:
     clear_v1()
     user_id1 = auth_register_v1("steve.smith@gmail.com", "my_good_password1", "Steve", "Smith")
-    user_id2 = auth_register_v1("james.smith12@gmail.com", "my_good_password1", "James", "Smith")
-    user_id3 = auth_register_v1("carl.johns56@gmail.com", "my_good_password1", "Carl", "Johns")
+    user_id2 = auth_register_v1("smith.james12@gmail.com", "my_good_password2", "Smith", "James")
+    user_id3 = auth_register_v1("carl.johns56@gmail.com", "my_good_password3", "Carl", "Johns")
     return [user_id1.get('auth_user_id'), user_id2.get('auth_user_id'), user_id3.get('auth_user_id')]
     
-
+# tests for channels_create
 def test_create_invalid_channel_shortname(example_user_id):
     with pytest.raises(InputError):
         channels_create_v1(example_user_id[0], "", False)
@@ -29,7 +29,6 @@ def test_create_channel_bad_user_id():
         channels_create_v1(67, "good_name", True)
     with pytest.raises(AccessError):
         channels_create_v1("bad_id", "good_name", True)
-
 
 def test_create_channel_single(example_user_id):
     new_channel_id = channels_create_v1(example_user_id[1], "I_love_seams", True)
@@ -52,6 +51,7 @@ def test_create_channel_multiple(example_user_id):
     new_channel_id = channels_create_v1(example_user_id[2], "second_cool_channel", True)
     assert new_channel_id.get('channel_id') == 4
 
+# tests for channel_invite
 # No channels created, so any channel id must be invalid.
 def test_channel_invite_invalid_channel_id(example_user_id):
     with pytest.raises(InputError):
@@ -75,3 +75,17 @@ def test_channel_invite_user_already_in_channel(example_user_id):
     channel_id = channels_create_v1(example_user_id[0], "Badgers", False)
     with pytest.raises(InputError):
         channel_invite_v1(example_user_id[0], channel_id.get('channel_id'), example_user_id[0])
+
+# tests for channel_details
+def test_detail_invalid_channel_id(example_user_id):
+    with pytest.raises(InputError):
+        channel_detail_v1(example_user_id[0], 1)
+
+def test_detail_bad_auth_id(example_user_id):
+    channel_id = channels_create_v1(example_user_id[0], "Badgers", False)
+    invalid_auth_id = sum(example_user_id) + 1
+    with pytest.raises(AccessError):
+        channels_detail_v1(invalid_auth_id, channel_id.get('channel_id'))
+
+
+
