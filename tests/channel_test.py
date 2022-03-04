3499d120cddadab9cb8c684703df822a3ddfad20
z5364817 <z5364817@cse.unsw.edu.au>
2022-03-05 02:16:26 +1100

    test: changes in the format of the output of tests

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -94,4 +94,8 @@ def test_detail_auth_id_not_member(example_user_id):
 
 def test_detail_correct_return_value(example_user_id):
     channel_id = channels_create_v1(example_user_id[0], "Badgers", True)
-    assert channel_details_v1(example_user_id[0], channel_id.get('channel_id')) == ("Badgers", True, example_user_id[0], example_user_id[0])
+    channel_details = channel_details_v1(example_user_id[0], channel_id.get('channel_id'))
+    assert channel_details['name'] == 'Badgers'
+    assert channel_details['is_public'] == True
+    assert channel_details['owner_members'] == 0
+    assert channel_details['all_members'] == [0]
