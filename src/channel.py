9738f1734d768a1a4d81c174571906e3cf0f39ef
z5364817 <z5364817@cse.unsw.edu.au>
2022-03-05 04:25:11 +1100

    dev: implemented channels_join_v1

--- src/channel.py
+++ src/channel.py
@@ -34,10 +34,10 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
     }
 
 def channel_details_v1(auth_user_id, channel_id):
-    if valid_channel_id(channel_id) != True:
+    if valid_channel_id(channel_id) == False:
         raise InputError("This channel_id does not correspond to an existing channel.")
     if valid_user_id(auth_user_id) == False:
-        raise AccessError("auth_user_id provided is not valid; this user does not exist.")
+        raise InputError("auth_user_id provided is not valid; this user does not exist.")
     if is_member(auth_user_id, channel_id) == False:
         raise AccessError("auth_user is not a member of the channel.")
 
@@ -71,5 +71,21 @@ def channel_messages_v1(auth_user_id, channel_id, start):
     }
 
 def channel_join_v1(auth_user_id, channel_id):
+    if valid_user_id(auth_user_id) == False:
+        raise InputError("auth_user_id provided is not valid; this user does not exist.")
+    if valid_channel_id(channel_id) == False:
+        raise InputError("This channel_id does not correspond to an existing channel.")
+    if is_member(auth_user_id, channel_id):
+        raise InputError("auth_user_id is already a member of the channel.")
+    
+    store = data_store.get()
+    for channel in store['channels']:
+        if channel['channel_id'] == channel_id:    
+            if channel['is_public'] == False:
+                raise AccessError(f"Access Denied. {channel['name']} is a private channel.")
+            else:
+                channel['user_ids'].append(auth_user_id)
+    data_store.set(store)
+    
     return {
     }
