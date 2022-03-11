20b2b64add80f5487609425f4a706ddd56ddc81d
z5364817 <z5364817@cse.unsw.edu.au>
2022-03-11 14:11:19 +1100

    dev: changes for iter1_rerun

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -103,7 +103,7 @@ def test_detail_invalid_channel_id(example_user_id):
 def test_detail_invalid_auth_id(example_user_id):
     channel_id = channels_create_v1(example_user_id[0], "Badgers", False)
     invalid_auth_id = sum(example_user_id) + 1
-    with pytest.raises(InputError):
+    with pytest.raises(AccessError):
         channel_details_v1(invalid_auth_id, channel_id.get('channel_id'))
 
 def test_detail_auth_id_not_member(example_user_id):
@@ -116,8 +116,8 @@ def test_detail_correct_return_value(example_user_id):
     channel_details = channel_details_v1(example_user_id[0], channel_id.get('channel_id'))
     assert channel_details['name'] == 'Badgers'
     assert channel_details['is_public'] == True
-    assert channel_details['owner_members'] == 0
-    assert channel_details['all_members'] == [0]
+    assert 0 in [k['u_id'] for k in channel_details['owner_members']]
+    assert 0 in [k['u_id'] for k in channel_details['all_members']]
 
 def test_detail_multiple_members(example_user_id):
     channel_id = channels_create_v1(example_user_id[0], "Badgers", True)
@@ -125,8 +125,9 @@ def test_detail_multiple_members(example_user_id):
     channel_details = channel_details_v1(example_user_id[0], channel_id.get('channel_id'))
     assert channel_details['name'] == 'Badgers'
     assert channel_details['is_public'] == True
-    assert channel_details['owner_members'] == 0
-    assert channel_details['all_members'] == [0, 1]
+    assert 0 in [k['u_id'] for k in channel_details['owner_members']]
+    assert 0 in [k['u_id'] for k in channel_details['all_members']]
+    assert 1 in [k['u_id'] for k in channel_details['all_members']]
 
 # tests for channel_join_v1
 def test_join_invalid_channel_id(example_user_id):
@@ -136,7 +137,7 @@ def test_join_invalid_channel_id(example_user_id):
 def test_join_invalid_auth_id(example_user_id):
     channel_id = channels_create_v1(example_user_id[0], "Badgers", False)
     invalid_auth_id = sum(example_user_id) + 1
-    with pytest.raises(InputError):
+    with pytest.raises(AccessError):
         channel_join_v1(invalid_auth_id, channel_id.get('channel_id'))
 
 def test_join_user_already_in_channel(example_user_id):
@@ -153,131 +154,10 @@ def test_join_success(example_user_id):
     channel_id = channels_create_v1(example_user_id[0], "Badgers", True)
     channel_join_v1(example_user_id[1], channel_id.get('channel_id'))
     channel_details = channel_details_v1(example_user_id[1], channel_id.get('channel_id'))
-    assert channel_details['all_members'] == [0, 1]
+    assert 0 in [k['u_id'] for k in channel_details['all_members']]
+    assert 1 in [k['u_id'] for k in channel_details['all_members']]
 
-# tests for channels_list_v1
-def test_no_channels(example_user_id):
-    channels = channels_list_v1(example_user_id[0])
-    assert channels['channels'] == []
 
-def test_list_one_channel_for_one_user(example_user_id):
-    channel_id = channels_create_v1(example_user_id[0], "Badgers", False)
-    channel_details = channel_details_v1(example_user_id[0], channel_id['channel_id'])
-    channels = channels_list_v1(example_user_id[0])
-    for c in channels['channels']:
-        assert c['name'] == channel_details['name']
-        assert c['channel_id'] == channel_id['channel_id']
-
-def test_list_one_channel_PER_USER_length(example_user_id):
-    channel_id1 = channels_create_v1(example_user_id[0], "Badgers", False)
-    channel_id2 = channels_create_v1(example_user_id[1], "Seams!", False)
-    channel_id3 = channels_create_v1(example_user_id[2], "No_name", False)
-    channel_details1 = channel_details_v1(example_user_id[0], channel_id1['channel_id'])
-    channel_details2 = channel_details_v1(example_user_id[1], channel_id2['channel_id'])
-    channel_details3 = channel_details_v1(example_user_id[2], channel_id3['channel_id'])
-    channels1 = channels_list_v1(example_user_id[0])
-    channels2 = channels_list_v1(example_user_id[1])
-    channels3 = channels_list_v1(example_user_id[2])
-    for c in channels1['channels']:
-        assert c['name'] == channel_details1['name']
-        assert c['channel_id'] == channel_id1['channel_id']
-    for c in channels2['channels']:
-        assert c['name'] == channel_details2['name']
-        assert c['channel_id'] == channel_id2['channel_id']
-    for c in channels3['channels']:
-        assert c['name'] == channel_details3['name']
-        assert c['channel_id'] == channel_id3['channel_id']
-
-    '''checking seperate lists to ensure they all only have one 
-    channel since they only are apart of one channel each'''
-    assert len(channels1['channels']) == 1
-    assert len(channels2['channels']) == 1
-    assert len(channels3['channels']) == 1
-
-def test_multi_length_for_one_user(example_user_id):
-    channel_id1 = channels_create_v1(example_user_id[0], "Badgers", False)
-    channel_id2 = channels_create_v1(example_user_id[0], "Seam!", False)
-    channel_id3 = channels_create_v1(example_user_id[0], "No_name", False)
-    channel_details1 = channel_details_v1(example_user_id[0], channel_id1['channel_id'])
-    channel_details2 = channel_details_v1(example_user_id[0], channel_id2['channel_id'])
-    channel_details3 = channel_details_v1(example_user_id[0], channel_id3['channel_id'])
-    channels = channels_list_v1(example_user_id[0])
-    
-    assert len(channels['channels']) == 3
-
-def test_multi_length_list_for_multiple_users(example_user_id):
-    channel_id1 = channels_create_v1(example_user_id[0], "Badgers", False)
-    channel_id2 = channels_create_v1(example_user_id[1], "Seams!", False)
-    channel_invite_v1(example_user_id[0], channel_id1['channel_id'], example_user_id[1])
-    channel_invite_v1(example_user_id[1], channel_id2['channel_id'], example_user_id[0])
-    channels1 = channels_list_v1(example_user_id[0])
-    channels2 = channels_list_v1(example_user_id[1])
-
-    '''checking seperate lists if function is adding channels correctly to seperate lists'''
-    assert len(channels1['channels']) == 2
-    assert len(channels2['channels']) == 2
-
-def test_showing_private_conversations_being_omitted(example_user_id):
-    channel_id1 = channels_create_v1(example_user_id[0], "Badgers", False)
-    channel_id2 = channels_create_v1(example_user_id[1], "Seams!", False)
-    channel_id3 = channels_create_v1(example_user_id[2], "No_name", False)
-    channel_invite_v1(example_user_id[0], channel_id1['channel_id'], example_user_id[1])
-    channel_invite_v1(example_user_id[0], channel_id1['channel_id'], example_user_id[2])
-    channel_invite_v1(example_user_id[1], channel_id2['channel_id'], example_user_id[2])
-    channels1 = channels_list_v1(example_user_id[0])
-    channels2 = channels_list_v1(example_user_id[1])
-    channels3 = channels_list_v1(example_user_id[2])
-    assert len(channels1['channels']) == 1
-    assert len(channels2['channels']) == 2
-    assert len(channels3['channels']) == 3
-
-# tests for channels_listall_v1
-'''Since channels_Listall_v1 is a variant of channels_list_v1, with same code excluding an 
-if statement, basic functionality testing is not as extensive for obvious reasons, key differences in the 
-functions will be tested, i.e private conversations being shown'''
-
-def test_private_conversations_being_listed(example_user_id):
-    channel_id1 = channels_create_v1(example_user_id[0], "Badgers", False)
-    channel_id2 = channels_create_v1(example_user_id[1], "Seams!", False)
-    channel_id3 = channels_create_v1(example_user_id[2], "No_name", False)
-    channel_invite_v1(example_user_id[0], channel_id1['channel_id'], example_user_id[1])
-    channel_invite_v1(example_user_id[0], channel_id1['channel_id'], example_user_id[2])
-    channel_invite_v1(example_user_id[1], channel_id2['channel_id'], example_user_id[2])
-    channels1 = channels_listall_v1(example_user_id[0])
-    channels2 = channels_listall_v1(example_user_id[1])
-    channels3 = channels_listall_v1(example_user_id[2])
-
-    '''Seperate users should all receive the same length list of channels'''
-    assert len(channels1['channels']) == 3
-    assert len(channels2['channels']) == 3
-    assert len(channels3['channels']) == 3
-    
-def test_no_channels_listall(example_user_id):
-    channels = channels_listall_v1(example_user_id[0])
-    assert channels['channels'] == []
-
-def test_list_channels_PER_USER_length(example_user_id):
-    channel_id1 = channels_create_v1(example_user_id[0], "Badgers", False)
-    channel_id2 = channels_create_v1(example_user_id[1], "Seams!", False)
-    channel_id3 = channels_create_v1(example_user_id[2], "No_name", False)
-    channel_details1 = channel_details_v1(example_user_id[0], channel_id1['channel_id'])
-    channel_details2 = channel_details_v1(example_user_id[1], channel_id2['channel_id'])
-    channel_details3 = channel_details_v1(example_user_id[2], channel_id3['channel_id'])
-    channels1 = channels_listall_v1(example_user_id[0])
-    channels2 = channels_listall_v1(example_user_id[1])
-    channels3 = channels_listall_v1(example_user_id[2])
-    all_channel_details = [channel_details1, channel_details2, channel_details3]
-
-    for (a,b) in zip(channels1['channels'], all_channel_details):
-        assert a['name'] == b['name']
-        assert a['channel_id'] == b['channel_id']
-
-    
-
-    '''checking seperate lists to ensure they all are 3, listall ignores private channels'''
-    assert len(channels1['channels']) == 3
-    assert len(channels2['channels']) == 3
-    assert len(channels3['channels']) == 3
 # Can't test the number of messages as there is no function to implement this yet.
 # Therefore, also can't test InputError when start is greater than the total number 
 # of messages in the channel.
@@ -459,3 +339,7 @@ def test_list_channels_PER_USER_length(example_user_id):
     assert len(channels1['channels']) == 3
     assert len(channels2['channels']) == 3
     assert len(channels3['channels']) == 3
+
+
+
+
