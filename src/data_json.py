ffd9c447b9d7b8b33852d7f8dd6afdbee5629005
Rumman Rauf <z5362562@ad.unsw.edu.au>
2022-03-12 21:30:18 +1100

    fix: fixed pylint errors

---
+++ src/data_json.py
@@ -0,0 +1,18 @@
+"""
+Module to implement the functionality allowing new data to be stored persistently.
+"""
+import json
+import os
+
+def write_data(data_store):
+    '''
+    Takes data from the data store and and writes it to data.json
+    '''
+    store = data_store.get()
+    path = os.path.dirname(__file__) + "../data/"
+    
+    with open(path + "channels.json", "w") as f:
+        json.dump(store["channels"], f)
+
+    with open(path + "users.json", "w") as f:
+        json.dump(store["users"], f)
