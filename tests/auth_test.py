2e388c313fb9f4d2ee949f61e91d5cba85e30f30
Laura Wrigley-Carr <rariera02@gmail.com>
2022-03-15 16:58:53 +1100

    Continued working on auth/v2 tests.

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,14 +1,40 @@
 import pytest
+import requests
+import json
 
 from src.auth import auth_register_v1, auth_login_v1
 from src.other import clear_v1
 from src.error import InputError
+from src import config
+
+def process_test_request(route, inputs):
+    requests.delete(config.url + "/clear/v1")
+   
+    # Return result of request.
+    response = requests.post(config.url + route, json = inputs)
+    return response.status_code
 
 
 def test_register_invalid_email():
-    clear_v1()
-    with pytest.raises(InputError):
-        auth_register_v1("defsanemail", "no_ats_oops", "Much", "Sense")
+    response = process_test_request(route="/auth/register/v2", inputs={'email': "defsanemail", 'password': "no_ats_oops", 'name_first': "Much", 'name_last': "Sense"})
+    assert(response == 400)
+
+"""
+def test_register_invalid_email2():
+        requests.delete(config.url + "/clear/v1")
+       
+        # Return result of request.
+        response = requests.post(config.url + "/auth/register/v2", json = {'email': "defsanemail", 'password': "no_ats_oops", 'name_first': "Much", 'name_last': "Sense"})
+
+        assert(response.status_code == 400)
+"""
+
+def test_echo():
+    '''
+    A simple test to check echo
+    '''
+    resp = requests.get(config.url + 'echo', params={'data': 'hello'})
+    assert json.loads(resp.text) == {'data': 'hello'}
 
 def test_register_duplicate_user():
     clear_v1()
