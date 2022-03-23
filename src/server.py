40a92a504211385e959283e97c51e6029fbbe990
Laura Wrigley-Carr <rariera02@gmail.com>
2022-03-23 18:09:40 +1100

    dev: removed debug flag.

--- src/server.py
+++ src/server.py
@@ -332,4 +332,4 @@ def handle_admin_changeperms():
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, quit_gracefully) # For coverage
-    APP.run(port=config.port, debug=True) # Do not edit this port #TODO
+    APP.run(port=config.port) # Do not edit this port
