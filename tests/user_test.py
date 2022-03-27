bec8f4840f06e34508e6cc2d7cc6c7aa476392d5
z5364817 <z5364817@cse.unsw.edu.au>
2022-03-28 03:12:59 +1100

    more tests for userpermission_change

--- tests/user_test.py
+++ tests/user_test.py
@@ -101,24 +101,59 @@ def test_user_sethandle_valid(example_user_id):
 # tests for admin/userpermission/change/v1
 def test_userpermission_change_invalid_u_id(example_user_id):
     invalid_user_id = sum(abs(d.get('auth_user_id')) for d in example_user_id) + 1
-    response = process_test_request(route="/admin/userpermission/change/v1", method='delete', inputs={
+    response = process_test_request(route="/admin/userpermission/change/v1", method='post', inputs={
         'token': example_user_id[0].get('token'), 
-        'u_id': invalid_user_id
+        'u_id': invalid_user_id,
+        'permission_id': 2
     })
     assert response.status_code == 400
 
 def test_userpermission_change_u_id_is_only_global_owner(example_user_id):
-    response = process_test_request(route="/admin/user/remove/v1", method='delete', inputs={
+    response = process_test_request(route="/admin/userpermission/change/v1", method='post', inputs={
         'token': example_user_id[0].get('token'), 
-        'u_id': example_user_id[0].get('auth_user_id')
+        'u_id': example_user_id[0].get('auth_user_id'), 
+        'permission_id': 2
     })
     assert response.status_code == 400
 
 def test_userpermission_change_invalid_permission_id(example_user_id):
-    pass
+    response = process_test_request(route="/admin/userpermission/change/v1", method='post', inputs={
+        'token': example_user_id[0].get('token'), 
+        'u_id': example_user_id[1].get('auth_user_id'), 
+        'permission_id': 3
+    })
+    assert response.status_code == 400
 
 def test_userpermission_change_same_permission_id(example_user_id):
-    pass
+    response = process_test_request(route="/admin/userpermission/change/v1", method='post', inputs={
+        'token': example_user_id[0].get('token'), 
+        'u_id': example_user_id[1].get('auth_user_id'), 
+        'permission_id': 2
+    })
+    assert response.status_code == 400
+
+def test_userpermission_change_not_global_owner(example_user_id):
+    response = process_test_request(route="/admin/userpermission/change/v1", method='post', inputs={
+        'token': example_user_id[1].get('token'), 
+        'u_id': example_user_id[2].get('auth_user_id'), 
+        'permission_id': 1
+    })
+    assert response.status_code == 403
+
+def test_userpermission_change_success(example_user_id):
+    response = process_test_request(route="/admin/userpermission/change/v1", method='post', inputs={
+        'token': example_user_id[0].get('token'), 
+        'u_id': example_user_id[1].get('auth_user_id'), 
+        'permission_id': 1
+    })
+    assert response.status_code == 200
+
+    response = process_test_request(route="/admin/userpermission/change/v1", method='post', inputs={
+        'token': example_user_id[1].get('token'), 
+        'u_id': example_user_id[0].get('auth_user_id'), 
+        'permission_id': 2
+    })
+    assert response.status_code == 200
 
 # NOTE: not an actual test - keep this at the bottom of the test file to clear data stores!
 def test_clear_data_stores():
