1ec04c529b531576cb21a05146c81ed6ba57c995
z5364817 <z5364817@cse.unsw.edu.au>
2022-04-01 09:12:47 +1100

    added minutes for 1st April

--- tests/user_test.py
+++ tests/user_test.py
@@ -45,6 +45,16 @@ def test_user_setemail_email_already_taken(example_user_id):
     user_info = get_user.json()
     response = process_test_request(route="/user/profile/setemail/v1", method='put', inputs={'token': example_user_id[1].get('token'), 'email': user_info['user'].get('email', None)})
     assert response.status_code == 400
+
+def test_user_setemail_valid_email(example_user_id):  
+    response = process_test_request(route="/user/profile/setemail/v1", method='put', inputs={'token': example_user_id[0].get('token'), 'email': "wearebadgers@gmail.com"})
+    assert response.status_code == 200
+    get_user = process_test_request(route="/user/profile/v1", method='get', inputs={
+        'token': example_user_id[0].get('token'), 
+        'u_id': example_user_id[0].get('auth_user_id')
+    })
+    user_info = get_user.json()
+    assert user_info['user']['email'] == "wearebadgers@gmail.com"
     
 # tests for user/profile/setname/v1
 def test_user_profile_setname_invalid_token(example_user_id):
