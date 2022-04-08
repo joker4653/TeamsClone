66909cfab60c5365ef37f43a6e0a1ecfbd60401e
z5364817 <z5364817@cse.unsw.edu.au>
2022-04-08 14:59:32 +1000

    created test files for standup functions

---
+++ tests/standup_tests/standup_active_test.py
@@ -0,0 +1,6 @@
+'''All test for standup/active/v1.'''
+
+import pytest
+import requests
+import json
+from tests.process_request import process_test_request
---
+++ tests/standup_tests/standup_start_test.py
@@ -0,0 +1,6 @@
+'''All test for standup/start/v1.'''
+
+import pytest
+import requests
+import json
+from tests.process_request import process_test_request
